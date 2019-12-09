bce1704dcdd01dff50f9c97d8b67dbd8fd3ec862
xscanpix <svante.n@live.se>
2019-12-09 13:12:16 +0100

    added counter test stubs

--- test/module_tests/test_counter.py
+++ test/module_tests/test_counter.py
@@ -1,6 +1,56 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python27
 
 import unittest
+from collections import Counter
+
+
+class TestCounterBlackbox(unittest.TestCase):
+
+    def test_counter_construct(self):
+        pass
+
+    def test_counter_f_elements(self):
+        pass
+
+    def test_counter_f_most_common(self):
+        pass
+
+    def test_counter_f_subtract(self):
+        pass
+
+    def test_counter_f_from_keys(self):
+        pass
+
+    def test_counter_f_update(self):
+        pass
+
+    def test_counter_to_set(self):
+        pass
+
+    def test_counter_to_list(self):
+        pass
+
+    def test_counter_to_dict(self):
+        pass
+
+    def test_counter_items(self):
+        pass
+
+    def test_counter_remove_zero_negative(self):
+        pass
+
+    def test_counter_addition_other(self):
+        pass
+
+    def test_counter_subtract_other(self):
+        pass
+
+    def test_counter_intersection_other(self):
+        pass
+
+    def test_counter_union_other(self):
+        pass
+
 
 
 if __name__ == '__main__':
